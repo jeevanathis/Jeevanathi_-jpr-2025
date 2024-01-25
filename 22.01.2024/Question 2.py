48b97df1ee56f21de9b5748df2a702c5c14c62c2
jeevanathis <144608851+jeevanathis@users.noreply.github.com>
2024-01-25 13:43:59 +0530

    Create Question 2.py

---
+++ 22.01.2024/Question 2.py
@@ -0,0 +1,22 @@
+"""
+Given an integer N, write a program to count the number of digits in N.
+
+Input Format
+
+Example 1: Input0: N = 12345
+
+Example 2: Input1: N = 8394
+
+Constraints
+
+n <= 10000
+
+Output Format
+
+Output0: 5 Explanation: N has 5 digits
+
+Output1: 4 Explanation: N has 4 digits
+"""
+N = int(input())
+num_digits = len(str(N))
+print(num_digits)
