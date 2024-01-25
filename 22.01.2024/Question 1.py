9abb9de7bc7bf0ffa48555d40591af8aaae0e0a7
jeevanathis <144608851+jeevanathis@users.noreply.github.com>
2024-01-25 13:41:18 +0530

    Create Question 1.py

---
+++ 22.01.2024/Question 1.py
@@ -0,0 +1,22 @@
+"""
+Given a number N reverse the number and print it.
+
+Example 1: Input: N = 123 Output: 321 Explanation: The reverse of 123 is 321
+
+Example 2: Input: N = 234 Output: 432 Explanation: The reverse of 234 is 432
+
+Input Format
+
+123
+
+Constraints
+
+N <= 1000
+
+Output Format
+
+32
+"""
+N = int(input())
+reverse_N = int(str(N)[::-1])
+print(reverse_N)
