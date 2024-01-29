796e88f6e0c2eba2868fc5dffe62f70591dd21e0
jeevanathis <144608851+jeevanathis@users.noreply.github.com>
2024-01-29 17:09:25 +0530

    Create Question 1.py

---
+++ 27.01.2004/Question 1.py
@@ -0,0 +1,47 @@
+Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+Example
+
+
+Return '12:01:00'.
+
+
+Return '00:01:00'.
+
+Function Description
+
+Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+timeConversion has the following parameter(s):
+
+string s: a time in  hour format
+Returns
+
+string: the time in  hour format
+Input Format
+
+A single string  that represents a time in -hour clock format (i.e.:  or ).
+Time conversion:
+from datetime import datetime
+
+def timeConversion(s):
+    # Write your code here
+    time_12h = datetime.strptime(s, "%I:%M:%S%p")
+    time_24h = time_12h.strftime("%H:%M:%S")
+    
+    return time_24h
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
